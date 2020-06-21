f36f1f044da8aa515da1c8fe7152208d0b77fb32
Chris Johnson <chrisjohnson00@hotmail.com>
2020-06-21 04:46:52 +0000

    Merged in kafka (pull request #1)
    Kafka Integration
    
    * Instead of watching and polling a directory, listen to a kafka topic to process the move
    Added support for specifying move type, one of tv or movie

--- app.py
+++ app.py
@@ -1,76 +1,93 @@
 import os
 import consul
 import shutil
-import time
 from datetime import datetime
 from app.file_matcher import get_show_file_parts, find_match, get_file_parts_for_directory
+from kafka import KafkaConsumer
+from json import loads
 
 CONFIG_PATH = "handbrake-file-mover"
 SLEEP_TIME = 61
 
 
 def main():
-    directory = get_watch_directory()
-    move_path = get_move_directory()
-    while True:
-        for filename in os.listdir(directory):
-            full_path = os.path.join(directory, filename)
-            file_size = get_file_size(full_path)
+    print("Starting!!", flush=True)
+    directory = "/watch"
+
+    consumer = KafkaConsumer(
+        'completedHandbrakeEncoding',
+        bootstrap_servers=['kafka-headless.kafka.svc.cluster.local:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id=get_consumer_group(),
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+    for message in consumer:
+        message_body = message.value
+        print("Processing new message {}".format(message_body), flush=True)
+
+        # message value should be an object with {'filename':'value",'type','tv|movie'}
+        # filename is from the kafka message value
+        filename = message_body['filename']
+        move_type = message_body['move_type']
+        full_path = os.path.join(directory, filename)
+
+        if move_type == "tv":
+            move_path = get_move_directory(move_type)
+            move_tv_show(filename, full_path, move_path)
+        elif move_type == "movies":
+            move_path = get_move_directory(move_type)
+            move_movie(filename, full_path, move_path)
+        else:
+            print("WARNING: There was no type in {}".format(message_body), flush=True)
+
+
+def move_movie(filename, full_path, move_path):
+    shutil.move(full_path, "{}/{}".format(move_path, filename))
+
+
+def move_tv_show(filename, full_path, move_path):
+    # break up the file into it's parts for easy comparison to original file to replace
+    source_file_parts = get_show_file_parts(filename)
+    # move_path/show/season
+    target_dir = "{}/{}/{}".format(move_path, source_file_parts['show'], source_file_parts['season'])
+    target_dir_exists = os.path.isdir(target_dir)
+    # let's hope that the original directory is found!
+    if target_dir_exists:
+        print(
+            "{} - Looking for matching file for '{}' in target directory '{}'".format(
+                datetime.now().strftime("%b %d %H:%M:%S"), filename,
+                target_dir),
+            flush=True)
+        file_to_replace = find_match(source_file_parts, get_file_parts_for_directory(target_dir))
+        if file_to_replace:
             print(
-                "{} - Found '{}' and it's size is {}".format(datetime.now().strftime("%b %d %H:%M:%S"), filename,
-                                                             file_size),
+                "{} - Replacing '{}' in target directory '{}' with '{}'".format(
+                    datetime.now().strftime("%b %d %H:%M:%S"), file_to_replace['filename'],
+                    target_dir, source_file_parts['filename']),
                 flush=True)
-            time.sleep(SLEEP_TIME)
-            # loop until the file size stops growing
-            while file_size != get_file_size(full_path):
-                print(
-                    "{} - File is still growing, waiting".format(datetime.now().strftime("%b %d %H:%M:%S")),
-                    flush=True)
-                file_size = get_file_size(full_path)
-                time.sleep(SLEEP_TIME)
-
-            # break up the file into it's parts for easy comparison to original file to replace
-            source_file_parts = get_show_file_parts(filename)
-            # move_path/show/season
-            target_dir = "{}/{}/{}".format(move_path, source_file_parts['show'], source_file_parts['season'])
-            target_dir_exists = os.path.isdir(target_dir)
-            # let's hope that the original directory is found!
-            if target_dir_exists:
-                print(
-                    "{} - Looking for matching file for '{}' in target directory '{}'".format(
-                        datetime.now().strftime("%b %d %H:%M:%S"), filename,
-                        target_dir),
-                    flush=True)
-                file_to_replace = find_match(source_file_parts, get_file_parts_for_directory(target_dir))
-                if file_to_replace:
-                    print(
-                        "{} - Replacing '{}' in target directory '{}' with '{}'".format(
-                            datetime.now().strftime("%b %d %H:%M:%S"), file_to_replace['filename'],
-                            target_dir, source_file_parts['filename']),
-                        flush=True)
-                    # remove the original file, then move the new one in place
-                    os.remove("{}/{}".format(target_dir, file_to_replace['filename']))
-                    shutil.move(full_path, "{}/{}".format(target_dir, source_file_parts['filename']))
-                else:
-                    print(
-                        "{} - Couldn't match any file in target directory '{}' for '{}'".format(
-                            datetime.now().strftime("%b %d %H:%M:%S"), target_dir, source_file_parts['filename']),
-                        flush=True)
-            else:
-                print(
-                    "{} - SKIPPING '{}', calculated target directory '{}' was not found!!".format(
-                        datetime.now().strftime("%b %d %H:%M:%S"), filename,
-                        target_dir),
-                    flush=True)
-        time.sleep(SLEEP_TIME)
+            # remove the original file, then move the new one in place
+            os.remove("{}/{}".format(target_dir, file_to_replace['filename']))
+            shutil.move(full_path, "{}/{}".format(target_dir, source_file_parts['filename']))
+        else:
+            print(
+                "{} - Couldn't match any file in target directory '{}' for '{}'".format(
+                    datetime.now().strftime("%b %d %H:%M:%S"), target_dir, source_file_parts['filename']),
+                flush=True)
+    else:
+        print(
+            "{} - SKIPPING '{}', calculated target directory '{}' was not found!!".format(
+                datetime.now().strftime("%b %d %H:%M:%S"), filename,
+                target_dir),
+            flush=True)
 
 
-def get_watch_directory():
-    return get_config("watch_directory_path")
+def get_consumer_group():
+    return get_config("consumer_group")
 
 
-def get_move_directory():
-    return get_config("move_directory_path")
+def get_move_directory(move_type):
+    return get_config("move_{}_directory_path".format(move_type))
 
 
 def get_file_size(file):
@@ -80,6 +97,7 @@ def get_file_size(file):
 def get_config(key, config_path=CONFIG_PATH):
     if os.environ.get(key):
         return os.environ.get(key)
+    print("looking for {}/{} in consul".format(config_path, key), flush=True)
     c = consul.Consul()
     index, data = c.kv.get("{}/{}".format(config_path, key))
     return data['Value'].decode("utf-8")
