237df94f69cd3815da9a253d2c49461320d591fb
Chris Johnson <csjohnson3@wsgc.com>
2020-07-11 22:30:15 -0700

    Adding flake8 to CI Updating the file mover to listen to a different topic, which is named more generic Adding a 3rd file mover logic Moving from python move/copy to `cp` and `mv`

--- app.py
+++ app.py
@@ -15,45 +15,52 @@ CONFIG_PATH = "handbrake-file-mover"
 def main():
     print("INFO: Starting!!", flush=True)
     start_http_server(8080)
-    directory = "/watch"
+    directory = get_config("output_directory_path")
 
     consumer = KafkaConsumer(
-        'completedHandbrakeEncoding',
+        get_config("KAFKA_TOPIC"),
         bootstrap_servers=['kafka-headless.kafka.svc.cluster.local:9092'],
         auto_offset_reset='earliest',
         enable_auto_commit=True,
         group_id=get_consumer_group(),
         value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-    file_discovered_metrics = Gauge('handbrake_job_move_file_in_process', 'File Mover Found A File')
+    file_discovered_metrics = Gauge('handbrake_job_move_file_in_process', 'File Mover Found A File',
+                                    labelnames=["move_type"])
     for message in consumer:
         message_body = message.value
-        print("INFO: Processing new message {}".format(message_body), flush=True)
-        file_discovered_metrics.inc()
-
         # message value should be an object with {'filename':'value",'type','tv|movie'}
-        # filename is from the kafka message value
-        filename = message_body['filename']
+        # but could also be: {'source_full_path': '/tv/The 100/Season 4/The 100 - S04E01 - Echoes WEBRip-1080p.mkv',
+        #     'move_type': 'to_encode', 'type': 'tv', 'quality': '1080p'}
+        # move_type is common between the all message types
         move_type = message_body['move_type']
-        full_path = os.path.join(directory, filename)
+        print("INFO: Processing new message {}".format(message_body), flush=True)
+        file_discovered_metrics.labels(move_type).inc()
 
-        if path.exists(full_path):
-            if move_type == "tv":
-                move_path = get_move_directory(move_type)
-                move_tv_show(filename, full_path, move_path)
-            elif move_type == "movies":
-                move_path = get_move_directory(move_type)
-                move_movie(filename, full_path, move_path)
-            else:
-                print("WARNING: There was no type in {}".format(message_body), flush=True)
+        if move_type == "tv":
+            # filename is from the kafka message value
+            filename = message_body['filename']
+            full_path = os.path.join(directory, filename)
+            move_path = get_move_directory(move_type)
+            move_tv_show(filename, full_path, move_path)
+        elif move_type == "movies":
+            # filename is from the kafka message value
+            filename = message_body['filename']
+            full_path = os.path.join(directory, filename)
+            move_path = get_move_directory(move_type)
+            move_movie(filename, full_path, move_path)
+        elif move_type == "to_encode":
+            copy_for_encoding(message_body)
+            exit("Um, I'm still testing")
         else:
-            print("WARNING: {} is not found on disk".format(full_path), flush=True)
+            print("WARNING: There was an invalid move_type in {}".format(message_body), flush=True)
+
         print("INFO: Done processing message {}".format(message_body), flush=True)
-        file_discovered_metrics.dec()
+        file_discovered_metrics.labels(move_type).dec()
 
 
 def move_movie(filename, full_path, move_path):
-    shutil.move(full_path, "{}/{}".format(move_path, filename))
+    move_file(full_path, "{}/{}".format(move_path, filename))
 
 
 def move_tv_show(filename, full_path, move_path):
@@ -80,9 +87,9 @@ def move_tv_show(filename, full_path, move_path):
                 target_file_full_path = "{}/{}".format(target_dir, source_file_parts['filename'])
                 original_file_full_path = "{}/{}".format(target_dir, file_to_replace['filename'])
                 if path.exists(target_file_full_path):
-                    shutil.move(full_path, target_file_full_path)
+                    move_file(full_path, target_file_full_path)
                 else:
-                    shutil.copyfile(full_path, target_file_full_path)
+                    copy_file(full_path, target_file_full_path)
                     os.remove(original_file_full_path)
             except Exception as e:
                 raise Exception("Could not copy {}, encountered Exception {}".format(full_path, e))
@@ -118,6 +125,33 @@ def get_config(key, config_path=CONFIG_PATH):
     c = consul.Consul()
     index, data = c.kv.get("{}/{}".format(config_path, key))
     return data['Value'].decode("utf-8")
+
+
+def copy_file(src, dest):
+    command = ["cp", src, dest]
+    print("File copy command called {}".format(command), flush=True)
+    # subprocess.run(command, check=True)
+
+
+def move_file(src, dest):
+    command = ["mv", src, dest]
+    print("File move command called {}".format(command), flush=True)
+    # subprocess.run(command, check=True)
+
+
+def copy_for_encoding(message_body):
+    """
+    Sample message_body: {'source_full_path': '/tv/The 100/Season 4/The 100 - S04E01 - Echoes WEBRip-1080p.mkv',
+    'move_type': 'to_encode', 'type': 'tv', 'quality': '1080p'}
+    :param message_body: The kafka message to process
+    :return:
+    """
+    copy_path = get_copy_path(message_body['quality'])
+    copy_file(message_body['source_full_path'], "{}/.".format(copy_path))
+
+
+def get_copy_path(quality):
+    return get_config("WATCH_{}".format(quality))
 
 
 if __name__ == '__main__':
